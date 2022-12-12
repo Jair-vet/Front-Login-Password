b527c80e41a6b0b3e9ebbcfec53b59ae54082c61
Jair <jairaceves56@gmail.com>
2022-12-12 17:03:51 -0600

    Inicio de la App

---
+++ Desktop/Vue/map-app/src/store/index.ts
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+interface State {
+  props?: boolean,
+  name: string
+}
+
+export default createStore<State>({
+  state: {
+    props: undefined,
+    name: ''
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
